e6a2bc2c4cd7d41a823c4c64a378d32a8d76acc0
Igor M <imcbell@outlook.com>
2019-08-20 13:10:37 +0300

    actix-web

--- src/bin/bf-server.rs
+++ src/bin/bf-server.rs
@@ -1,4 +1,26 @@
-/// TODO
-/// Webserver that will present an interface to Bullfinch scraper
 
-fn main() {}
+use actix_web::{web, App, Responder, HttpServer, HttpResponse};
+
+
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+fn main() {
+    println!("Server running.");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap();
+}
